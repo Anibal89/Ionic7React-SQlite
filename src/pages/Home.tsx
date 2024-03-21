3701f29230d5cd14880b3d33bb1af296e340c78e
Alterlapsus <israel.delen96@gmail.com>
2024-03-21 11:35:21 -0600

    Se realizan cambios en el color de las máquinas y se mejora el diseño del ingreso de usuarios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonList } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonList,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import { key, personCircle } from "ionicons/icons";
 import "./Home.css";
 import useSQLiteDB from "../composables/useSQLiteDB";
 import useServerDataSender from "../composables/useServerDataSender";
-import useConfirmationAlert from "../composables/useConfirmationAlert"; 
+import useConfirmationAlert from "../composables/useConfirmationAlert";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
-
 
 interface UserItem {
   id: number;
@@ -15,11 +30,10 @@ interface UserItem {
 
 const Home: React.FC = () => {
   const [items, setItems] = useState<UserItem[]>([]);
-  const [userId, setUserId] = useState<number | ''>('');
-  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState<number | "">("");
+  const [userName, setUserName] = useState("");
   const { initialized, addUser, deleteUser, loadData } = useSQLiteDB();
   const { showConfirmationAlert, ConfirmationAlert } = useConfirmationAlert();
-
 
   const fetchUsers = async () => {
     if (initialized) {
@@ -30,18 +44,18 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     fetchUsers();
-  }, [initialized]); // Se llama a fetchUsers cuando el componente se monta o se actualiza el estado initialized
+  }, [initialized]);
 
   const handleSubmit = async () => {
-    if (userId === '' || !userName) {
+    if (userId === "" || !userName) {
       console.error("Both ID and name are required");
       return;
     }
     try {
       await addUser(Number(userId), userName);
-      setUserId('');
-      setUserName('');
-      await fetchUsers(); // Recarga los datos después de añadir un usuario
+      setUserId("");
+      setUserName("");
+      await fetchUsers();
     } catch (error) {
       console.error("Error submitting user:", error);
     }
@@ -49,74 +63,78 @@ const Home: React.FC = () => {
 
   const handleDelete = (id: number) => {
     showConfirmationAlert(
-      "Are you sure you want to delete this user?", // Mensaje de confirmación
-      () => deleteUserConfirmed(id) // Acción a confirmar
+      "Are you sure you want to delete this user?",
+      () => deleteUserConfirmed(id)
     );
   };
-
 
   const deleteUserConfirmed = async (id: number) => {
     try {
       await deleteUser(id);
-      await fetchUsers(); // Recarga los datos después de eliminar un usuario
+      await fetchUsers();
     } catch (error) {
       console.error("Error deleting user:", error);
     }
   };
 
-  // useServerDataSender({ items }, 15000); 
-  // const history = useHistory();
-
-  // const regresar = ()=>{
-    
-  // }
-
-
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Unisa Producción</IonTitle>
+          <IonTitle>Unisa Producción Usuarios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen class="ion-padding">
-      {/* <IonButton onClick={regresar} color={'dark'}>Ir Usuarios</IonButton> */}
-      <a  style={{padding:'4px 25px', background:'black', color:'white', borderRadius:'4px', textDecoration:'none', border: '1px solid black'}}  href="/ExtruderStatusPage">Regresar</a>
-    
-        <div className="ion-padding">
-          <IonItem>
-            <IonInput
-              type="number"
-              value={userId.toString()}
-              onIonChange={(e) => setUserId(e.detail.value ? Number(e.detail.value) : '')}
-              clearInput
-              label="User ID:"
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput
-              value={userName}
-              onIonChange={(e) => setUserName(e.detail.value!)}
-              clearInput
-              label="User Name:"
-            ></IonInput>
-          </IonItem>
-          <IonButton expand="block" onClick={handleSubmit}>
-            Add User
-          </IonButton>
-          <IonList>
-            {items.map((item) => (
-              <IonItem key={item.id}>
-                <IonLabel>
-                  ID: {item.id}, Name: {item.name}
-                </IonLabel>
-                <IonButton onClick={() => handleDelete(item.id)} color="danger">
-                  Delete
-                </IonButton>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="6">
+              {/* Formulario de inicio de sesión */}
+              <IonTitle className="ion-text-center">Agregar Usuario</IonTitle>
+              <IonItem>
+                <IonIcon slot="start" icon={key} />
+                <IonLabel position="floating">ID Usuario:</IonLabel>
+                <IonInput
+                  type="number"
+                  value={userId.toString()}
+                  onIonChange={(e) => setUserId(e.detail.value ? Number(e.detail.value) : "")}
+                  clearInput
+                ></IonInput>
               </IonItem>
-            ))}
-          </IonList>
-        </div>
+
+              <IonItem>
+                <IonIcon slot="start" icon={personCircle} />
+                <IonLabel position="floating">Nombre Usuario:</IonLabel>
+                <IonInput
+                  value={userName}
+                  onIonChange={(e) => setUserName(e.detail.value!)}
+                  clearInput
+                ></IonInput>
+              </IonItem>
+
+              <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+               <IonButton size="small" onClick={handleSubmit}>Añadir Usuario</IonButton>
+              </div>
+
+            </IonCol>
+
+            <IonCol size="6">
+              {/* Listado de usuarios */}
+              <IonTitle className="ion-text-center">Lista De Usuarios</IonTitle>
+              <IonList>
+                {items.map((item) => (
+                  <IonItem key={item.id}>
+                    <IonLabel>
+                      <strong>ID:</strong> {item.id}, <strong>Nombre:</strong> {item.name}
+                    </IonLabel>
+                    <IonButton onClick={() => handleDelete(item.id)} color="danger">
+                      Eliminar
+                    </IonButton>
+                  </IonItem>
+                ))}
+              </IonList>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
         {ConfirmationAlert}
       </IonContent>
     </IonPage>
